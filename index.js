eb8bf5ecba7a0eef251636337776f05e6b226423
BlessingEmejulu <blessingnneamaka57@gmail.com>
2025-01-10 00:17:15 -0800

    Implement real-time updates for shopping list and clean up initial items

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import {initializeApp}  from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
-import {getDatabase, ref, push}  from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+import {getDatabase, ref, push, onValue}  from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
 
 const appSettings = {
     databaseURL: "https://shopping-cat-7984d-default-rtdb.firebaseio.com/"
@@ -18,11 +18,25 @@ addButtonEl.addEventListener("click", function() {
     let inputValue = inputFieldEl.value
      
     push(shoppingListInDB, inputValue)
-    console.log(inputValue)
 
-    appendItemToShoppingListEl(inputValue)})
     clearInputFieldEl()
+  })
 
+    onValue(shoppingListInDB, function(snapshot) {
+      clearShoppingListEl()
+      let itemArray = Object.values(snapshot.val())
+      
+    
+      for (let i = 0; i < itemArray.length; i++) {
+        appendItemToShoppingListEl(itemArray[i])
+      }
+    })
+
+
+    function clearShoppingListEl() {
+      shoppingListEl.innerHTML = ""
+  }
+  
 function clearInputFieldEl() {
     inputFieldEl.value = ""
 }
